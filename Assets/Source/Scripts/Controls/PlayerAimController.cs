df8762cd1c6c473c805f854678eaccaf8186e6bd
Victor <wionvictor@gmail.com>
2022-01-29 14:38:42 +0500

    AimTrajectory

--- Assets/Source/Scripts/Controls/PlayerAimController.cs
+++ Assets/Source/Scripts/Controls/PlayerAimController.cs
@@ -26,7 +26,20 @@ namespace GGJ2022.Source.Scripts.Controls
 
         private void AimTrajectory()
         {
-            
+            Vector2 firePos = new Vector2(
+                _controlInfo.FireJoystick.Horizontal * 100f + Input.GetAxis("Horizontal") * 100f,
+                _controlInfo.FireJoystick.Vertical * 100f + Input.GetAxis("Vertical") * 100f);
+            if (firePos.normalized.magnitude < 1 && _lineRenderer.enabled)
+            {
+                _lineRenderer.enabled = false;
+                return;
+            }
+
+            if (!_lineRenderer.enabled)
+            {
+                _lineRenderer.enabled = true;
+            }
+            _lineRenderer.SetPosition(1, new Vector3(1, 1, 1 ) * firePos);
         }
     }
 }
